4519de982390ec191578ada960595b87fce58abf
문슬범 <msi0@naver.com>
2023-06-01 16:44:38 +0900

    20230601-1644

---
+++ ch13-MvcPage/src/main/java/kr/board/dao/BoardDAO.java
@@ -0,0 +1,138 @@
+package kr.board.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.board.vo.BoardVO;
+import kr.util.DBUtil;
+
+public class BoardDAO {
+	//싱글턴 패턴
+	private static BoardDAO instance = new BoardDAO();
+	public static BoardDAO getInstance() {
+		return instance;
+	}
+	private BoardDAO() {}
+	
+	//글 등록
+	public void insertBoard(BoardVO board) throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		try {
+			//커넥션풀로부터 커넥션을 할당
+			conn = DBUtil.getConnection();
+			//SQL문 작성
+			sql = "INSERT INTO zboard (board_num,"
+					+ "title,content,filename,ip,mem_num) "
+					+ "VALUES (zboard_seq.nextval,?,?,?,?,?)";
+			//PreparedStatement 객체 생성
+			pstmt = conn.prepareStatement(sql);
+			//?에 데이터 바인딩
+			pstmt.setString(1, board.getTitle());
+			pstmt.setString(2, board.getContent());
+			pstmt.setString(3, board.getFilename());
+			pstmt.setString(4, board.getIp());
+			pstmt.setInt(5, board.getMem_num());
+
+			//SQL문 실행
+			pstmt.executeUpdate();
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			//자원정리
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	}
+	//총 레코드 수(검색레코드 수)
+	public int getBoardCount(String keyfield,String keyword)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		String sub_sql = "";
+		int count = 0;
+		
+		try {
+			//커넥션 풀로부터 커넥션을 할당받음
+			conn = DBUtil.getConnection();
+			//SQL문 작성
+			sql = "SELECT COUNT(*) FROM zboard b "
+					+ "JOIN zmember m USING(mem_num)";
+			//PreparedStatement 객체 생성
+			pstmt = conn.prepareStatement(sql);
+			
+			//SQL 실행
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				count = rs.getInt(1); //컬럼인덱스를 넣어줌
+			}
+			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			//자원정리
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		return count;
+	}
+	
+	//글 목록(검색글 목록)
+	public List<BoardVO> getListBoard(int start, int end, 
+								String keyfield, String keyword) throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<BoardVO> list = null;
+		String sql = null;
+		String sub_sql = "";
+		
+		try {
+			//커넥션 풀로부터 커넥션을 할당받음
+			conn = DBUtil.getConnection();
+			//SQL문 작성
+			sql = "SELECT * FROM (SELECT a.*, "
+					+ "rownum rnum FROM (SELECT * "
+					+ "FROM zboard b JOIN zmember m "
+					+ "USING(mem_num) ORDER BY "
+					+ "b.board_num DESC)a) "
+					+ "WHERE rnum>=? AND rnum<=?";
+			//PreparedStatement 객체 생성
+			pstmt = conn.prepareStatement(sql);
+			//?에 데이터 바인딩
+			pstmt.setInt(1, start);
+			pstmt.setInt(2, end);
+
+			//SQL문 실행
+			rs = pstmt.executeQuery();
+			list = new ArrayList<BoardVO>();
+			while(rs.next()) {
+				BoardVO board = new BoardVO();
+				board.setBoard_num(rs.getInt("board_num"));
+				board.setTitle(rs.getString("title"));
+				board.setHit(rs.getInt("hit"));
+				board.setReg_date(rs.getDate("reg_date"));
+				board.setId(rs.getString("id"));
+				
+				//자바빈을 ArrayList에 저장
+				list.add(board);
+			}
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			//자원정리
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		return list;
+	}
+	//글 상세
+	//조회수 증가
+	//파일 삭제
+	//글 수정
+	//글 삭제
+	
+	
+}
