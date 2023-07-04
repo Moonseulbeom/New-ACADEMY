5366910672a81c37e6a2033dcfe5a1f9e50e2aa6
sinet <sinet@DESKTOP-7H06HNO>
2023-07-04 17:29:44 +0900

    230704_오후 커밋 2

--- ch01DI/src/main/java/kr/spring/ch08/WorkController.java
+++ ch01DI/src/main/java/kr/spring/ch08/WorkController.java
@@ -12,7 +12,10 @@ public class WorkController {
 		this.email = email;
 	}
 	
-	
+	@Override
+	public String toString() {
+		return "WorkController [periodTime=" + periodTime + ", email=" + email + "]";
+	}
 	
 	
 	
