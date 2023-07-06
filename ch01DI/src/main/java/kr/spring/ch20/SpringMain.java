b8e9d5ea02e3439083caeaadd4d3a806b5209997
sinet <sinet@DESKTOP-7H06HNO>
2023-07-06 11:41:38 +0900

    230706_1차 커밋

--- ch01DI/src/main/java/kr/spring/ch20/SpringMain.java
+++ ch01DI/src/main/java/kr/spring/ch20/SpringMain.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.support.AbstractApplicationContext;
 
 public class SpringMain {
-	public static void main(String[] args) {
+	public static void main(String[] args) {//컨테이너 불러오는 코드도 달라짐
 		//컨테이너 생성
 		AbstractApplicationContext context = 
 				new AnnotationConfigApplicationContext(
